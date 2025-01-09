4a75d0f606daa4d43d079373acf5da01923a3107
kartemdev <ihctuyd@mail.ru>
2025-01-09 13:53:00 +0300

    add async pattern retry

---
+++ osmani-patterns-2nd/async-patterns/promise-retry.js
@@ -0,0 +1,29 @@
+
+const makeRequestWithRetry = (url) => {
+  let attempts = 0;
+  
+  const makeRequest = () => {
+    return new Promise((resolve, reject) => (
+      fetch(url)
+        .then((response) => response.json())
+        .then((data) => resolve(data))
+        .catch((error) => reject(error))
+    ));
+  };
+  
+  const retry = () => {
+    attempts++;
+
+    if (attempts >= 3) {
+      throw new Error('Request failed after 3 attempts.');
+    }
+  
+    console.log(`Retrying request: attempt ${attempts}`);
+  
+    return makeRequest().catch(retry)
+  };
+
+  return makeRequest().catch(retry);
+}
+
+makeRequestWithRetry('https://test.example/retry');
